dd51e924387bd14ef06407e508b39ae587122372
ayaanqui <ayaanqui.com@gmail.com>
2025-03-06 17:10:28 -0600

    feat: add flat list + bug fixes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import { useLazyQuery } from '@apollo/client';
 import { AlertTriangle } from 'lucide-react-native';
-import { useContext, useEffect } from 'react';
-import { View, ScrollView, RefreshControl } from 'react-native';
+import { useContext, useEffect, useState } from 'react';
+import { View, RefreshControl, FlatList, SafeAreaView } from 'react-native';
 
 import ProductItem, { ProductLoadingItem } from '@/components/ProductItem';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/Alert';
@@ -12,10 +12,12 @@ export default function HomeScreen() {
   const { token } = useContext(UserAuthContext);
   const [getAllProducts, { data: productsData, loading: productsLoading, error: productsError }] =
     useLazyQuery(AllProductsDocument);
+  const [refreshing, setRefreshing] = useState(false);
 
-  function fetchAllProducts() {
-    getAllProducts({
+  function fetchAllProducts(refresh?: boolean) {
+    return getAllProducts({
       context: { headers: { authorization: `Bearer ${token}` } },
+      fetchPolicy: refresh ? 'network-only' : undefined,
     });
   }
 
@@ -24,26 +26,44 @@ export default function HomeScreen() {
   }, []);
 
   return (
-    <ScrollView>
-      <View className="p-5 py-36">
-        {productsError && (
+    <SafeAreaView>
+      {productsError && (
+        <View className="p-5">
           <Alert icon={AlertTriangle} variant="destructive" className="mb-10 max-w-xl">
             <AlertTitle>Error!</AlertTitle>
             <AlertDescription>{productsError.message}</AlertDescription>
           </Alert>
-        )}
-        {productsLoading &&
-          Array(10)
+        </View>
+      )}
+      {(productsLoading || refreshing) && (
+        <View className="p-5">
+          {Array(10)
             .fill(0)
-            .map((_, i) => <ProductLoadingItem key={i} />)}
-
-        <View className="max-w-full">
-          {productsData &&
-            productsData.allProducts.map((product) => (
-              <ProductItem product={product} key={product.code} />
+            .map((_, i) => (
+              <ProductLoadingItem key={i} />
             ))}
         </View>
-      </View>
-    </ScrollView>
+      )}
+
+      <FlatList
+        data={productsData?.allProducts ?? []}
+        keyExtractor={({ id }) => id}
+        renderItem={({ item }) => <ProductItem product={item} />}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={() => {
+              setRefreshing(true);
+              setTimeout(() => {
+                fetchAllProducts(true).finally(() => setRefreshing(false));
+              }, 2000);
+            }}
+            colors={['grey']}
+            progressBackgroundColor="black"
+          />
+        }
+        className="p-5"
+      />
+    </SafeAreaView>
   );
 }
