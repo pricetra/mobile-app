0091a50b6cb484fd56d4c5be3bda0193f627e796
ayaanqui <ayaanqui.com@gmail.com>
2025-03-15 02:28:45 -0500

    feat: add textarea component

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -9,7 +9,7 @@ const Input = forwardRef<ElementRef<typeof TextInput>, ComponentPropsWithoutRef<
       <TextInput
         ref={ref}
         className={cn(
-          'native:h-12 native:text-lg native:leading-[1.25] placeholder:text-muted-foreground web:ring-offset-background web:focus-visible:ring-ring h-10 rounded-md border border-gray-300 bg-white px-3 text-base text-black file:border-0 file:bg-transparent file:font-medium web:flex web:w-full web:py-2 web:focus-visible:outline-none web:focus-visible:ring-1 web:focus-visible:ring-offset-0 lg:text-sm',
+          'native:h-12 native:text-lg native:leading-[1.25] h-10 text-nowrap rounded-md border border-gray-300 bg-white px-3 text-base file:border-0 file:bg-transparent file:font-medium placeholder:text-muted-foreground web:flex web:w-full web:py-2 web:ring-offset-background web:focus-visible:outline-none web:focus-visible:ring-1 web:focus-visible:ring-ring web:focus-visible:ring-offset-0 lg:text-sm',
           props.editable === false && 'opacity-50 web:cursor-not-allowed',
           className
         )}
--- components/ui/ModalFormMini.tsx
+++ components/ui/ModalFormMini.tsx
@@ -31,7 +31,7 @@ export default function ModalFormMini({
       <Pressable
         className="h-full"
         onPress={onRequestClose}
-        style={{ backgroundColor: visible ? 'rgba(0,0,0,.3)' : 'rgba(0,0,0,0)' }}
+        style={{ backgroundColor: visible ? 'rgba(0,0,0,.7)' : 'rgba(0,0,0,0)' }}
       />
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
