cea69d84774b3d880bebcc886504825fbd6e7fcd
ayaanqui <ayaanqui.com@gmail.com>
2025-04-27 00:59:44 -0500

    feat: changes

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,13 @@
 import { useMutation } from '@apollo/client';
-import { upload } from 'cloudinary-react-native';
-import { randomUUID } from 'expo-crypto';
 import * as ImagePicker from 'expo-image-picker';
 import { useContext } from 'react';
-import { SafeAreaView, View, ScrollView, Alert } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 
 import AllProductBillingData from '@/components/profile/AllProductBillingData';
 import ProfileSmall from '@/components/profile/ProfileSmall';
 import { UserAuthContext } from '@/context/UserContext';
 import { UpdateProfileDocument, User } from '@/graphql/types/graphql';
-import { cloudinary } from '@/lib/files';
+import { getApolloUploadObject } from '@/lib/files';
 
 export default function ProfileScreen() {
   const { user, updateUser, logout } = useContext(UserAuthContext);
@@ -28,35 +26,53 @@ export default function ProfileScreen() {
     if (result.canceled || result.assets.length === 0) return;
 
     const picture = result.assets.at(0);
-    if (!picture || !picture.uri) return alert('could not process image');
+    if (!picture || !picture.uri) return;
 
-    const upload_id = randomUUID();
-    upload(cloudinary, {
-      file: picture.uri,
-      options: {
-        public_id: upload_id,
-        tags: ['USER_PROFILE'],
+    const file = getApolloUploadObject(picture);
+    console.log(file);
+    updateProfileDocument({
+      variables: {
+        input: {
+          avatarFile: file,
+        },
       },
-      callback: (err) => {
-        if (err) {
-          Alert.alert('Upload Failed', err.message);
+    })
+      .then(({ errors, data }) => {
+        if (errors || !data) {
+          console.error(errors);
           return;
         }
-        updateProfileDocument({
-          variables: {
-            input: {
-              avatar: upload_id,
-            },
-          },
-        }).then(({ errors, data }) => {
-          if (errors || !data) {
-            console.error(errors);
-            return;
-          }
-          updateUser({ ...data.updateProfile } as User);
-        });
-      },
-    });
+        updateUser({ ...data.updateProfile } as User);
+      })
+      .catch((err) => console.error(err));
+
+    // const upload_id = randomUUID();
+    // upload(cloudinary, {
+    //   file: picture.uri,
+    //   options: {
+    //     public_id: upload_id,
+    //     tags: ['USER_PROFILE'],
+    //   },
+    //   callback: (err) => {
+    //     if (err) {
+    //       Alert.alert('Upload Failed', err.message);
+    //       return;
+    //     }
+    //     updateProfileDocument({
+    //       variables: {
+    //         input: {
+    //           avatarFile: picture.uri,
+    //         },
+    //       },
+    //     }).then(({ errors, data }) => {
+    //       if (errors || !data) {
+    //         console.error(errors);
+    //         return;
+    //       }
+    //       updateUser({ ...data.updateProfile } as User);
+    //     });
+    //   },
+    // });
   }
 
   return (
