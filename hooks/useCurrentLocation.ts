cfd655bdd901e3b3f1b32daa277690b38cb6bda8
ayaanqui <ayaanqui.com@gmail.com>
2025-05-08 18:02:20 -0500

    perf: improve location detection

--- hooks/useCurrentLocation.ts
+++ hooks/useCurrentLocation.ts
@@ -4,22 +4,30 @@ import { useEffect, useState } from 'react';
 export default function useCurrentLocation() {
   const [location, setLocation] = useState<Location.LocationObject>();
 
-  async function getCurrentLocation() {
+  async function requestPermission() {
     const { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== 'granted') {
       return alert('Permission to access location was denied');
     }
-    Location.watchPositionAsync(
-      {
-        timeInterval: 1000 * 60,
-      },
-      setLocation
-    );
+  }
+
+  async function getCurrentLocation(options: Location.LocationOptions) {
+    const curLocation = await Location.getCurrentPositionAsync(options);
+    setLocation(curLocation);
+    return curLocation;
+  }
+
+  function watchLocation(options: Location.LocationOptions, cb: Location.LocationCallback) {
+    return Location.watchPositionAsync(options, (l) => {
+      setLocation(l);
+      cb(l);
+    });
   }
 
   useEffect(() => {
-    getCurrentLocation();
+    requestPermission();
+    getCurrentLocation({});
   }, []);
 
-  return { location };
+  return { location, getCurrentLocation, watchLocation };
 }
