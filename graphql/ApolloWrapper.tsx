4425a34be20e18fdc6089396448ecf6af92f505c
ayaanqui <ayaanqui.com@gmail.com>
2025-05-19 13:29:28 -0500

    feat: apollo retry

--- graphql/ApolloWrapper.tsx
+++ graphql/ApolloWrapper.tsx
@@ -9,12 +9,26 @@ import { setContext } from '@apollo/client/link/context';
 import createUploadLink from 'apollo-upload-client/createUploadLink.mjs';
 import { ReactNode, useContext, useEffect, useState } from 'react';
 
+import { RetryLink } from '@apollo/client/link/retry';
+
 import { JwtStoreContext } from '@/context/JwtStoreContext';
 
 const uri = process.env.EXPO_PUBLIC_API_URL ?? 'http://localhost:8080/graphql';
 
 const httpLink = createHttpLink({ uri });
 const uploadLink = createUploadLink({ uri });
+
+const retryLink = new RetryLink({
+  delay: {
+    initial: 1000,
+    max: Infinity,
+    jitter: true,
+  },
+  attempts: {
+    max: 5,
+    retryIf: (error, _operation) => !!error,
+  },
+});
 
 function createAuthLink(jwt?: string) {
   return setContext((_, { headers }) => {
@@ -29,7 +43,7 @@ function createAuthLink(jwt?: string) {
 
 function newClient(jwt?: string) {
   return new ApolloClient({
-    link: createAuthLink(jwt).concat(uploadLink),
+    link: createAuthLink(jwt).concat(uploadLink).concat(retryLink),
     cache: new InMemoryCache(),
   });
 }
