f42558144cbf9f89b5edba76c173fe317dccb870
ayaanqui <ayaanqui.com@gmail.com>
2025-05-30 13:30:56 -0500

    fix: add keyboard avoiding view to fullscreen modal

--- components/ui/ModalFormFull.tsx
+++ components/ui/ModalFormFull.tsx
@@ -1,5 +1,13 @@
 import { Feather } from '@expo/vector-icons';
-import { Modal, TouchableOpacity, View, Text, SafeAreaView, FlatList } from 'react-native';
+import {
+  Modal,
+  TouchableOpacity,
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  KeyboardAvoidingView,
+} from 'react-native';
 import { AutocompleteDropdownContextProvider } from 'react-native-autocomplete-dropdown';
 
 import { cn } from '@/lib/utils';
@@ -19,34 +27,36 @@ export default function ModalFormFull({
     <Modal animationType="slide" visible={visible} onRequestClose={onRequestClose}>
       <AutocompleteDropdownContextProvider>
         <View className="bg-white">
-          <SafeAreaView>
-            <View className="flex flex-row items-center justify-between gap-3 border-b-[1px] border-gray-100 pt-2">
-              {TitleComponent ? (
-                <View className="px-5 py-3">{TitleComponent}</View>
-              ) : (
-                <Text className="flex flex-1 flex-row items-center gap-2 px-7 py-5 text-2xl font-bold">
-                  {title}
-                </Text>
-              )}
+          <KeyboardAvoidingView behavior="padding">
+            <SafeAreaView>
+              <View className="flex flex-row items-center justify-between gap-3 border-b-[1px] border-gray-100">
+                {TitleComponent ? (
+                  <View className="px-5 py-3">{TitleComponent}</View>
+                ) : (
+                  <Text className="flex flex-1 flex-row items-center gap-2 px-7 py-3 text-xl font-bold">
+                    {title}
+                  </Text>
+                )}
 
-              <TouchableOpacity
-                onPress={onRequestClose}
-                className="flex items-center justify-center p-5">
-                <Feather name="x" size={25} />
-              </TouchableOpacity>
-            </View>
-            <FlatList
-              data={[{ key: 'form' }]} // Dummy data to render form
-              keyExtractor={(item) => item.key}
-              renderItem={() => <>{children}</>}
-              ListHeaderComponent={<View className="h-5" />}
-              ListFooterComponent={<View className="h-5" />}
-              keyboardShouldPersistTaps="handled"
-              contentInsetAdjustmentBehavior="automatic"
-              className={cn('px-5', className)}
-              nestedScrollEnabled
-            />
-          </SafeAreaView>
+                <TouchableOpacity
+                  onPress={onRequestClose}
+                  className="flex items-center justify-center px-5 py-3">
+                  <Feather name="x" size={25} />
+                </TouchableOpacity>
+              </View>
+              <FlatList
+                data={[{ key: 'form' }]} // Dummy data to render form
+                keyExtractor={(item) => item.key}
+                renderItem={() => <>{children}</>}
+                ListHeaderComponent={<View className="h-5" />}
+                ListFooterComponent={<View className="h-5" />}
+                keyboardShouldPersistTaps="handled"
+                contentInsetAdjustmentBehavior="automatic"
+                className={cn('px-5', className)}
+                nestedScrollEnabled
+              />
+            </SafeAreaView>
+          </KeyboardAvoidingView>
         </View>
       </AutocompleteDropdownContextProvider>
     </Modal>
