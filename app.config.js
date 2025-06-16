a8e4f73045aa32764794e010fc97f48b7d7310bf
ayaanqui <ayaanqui.com@gmail.com>
2025-06-16 04:05:12 -0500

    build: bump to v1.0.10

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ const IS_DEV = process.env.APP_VARIANT === 'development';
 export default {
   name: IS_DEV ? 'Pricetra (Dev)' : 'Pricetra',
   slug: 'pricetra',
-  version: '1.0.9',
+  version: '1.0.10',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'pricetra',
