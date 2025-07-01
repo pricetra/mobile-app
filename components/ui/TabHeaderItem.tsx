7b8eb6a46e009844aa590351cecb93def3a2bbef
ayaanqui <ayaanqui.com@gmail.com>
2025-06-30 20:35:31 -0500

    feat: show search on header item

--- components/ui/TabHeaderItem.tsx
+++ components/ui/TabHeaderItem.tsx
@@ -1,14 +1,18 @@
-import { Feather } from '@expo/vector-icons';
+import { Feather, Ionicons } from '@expo/vector-icons';
 import { BottomTabHeaderProps } from '@react-navigation/bottom-tabs';
 import { router } from 'expo-router';
-import { ReactNode } from 'react';
+import { ReactNode, useContext, useState } from 'react';
 import { Platform, SafeAreaView, View, TouchableOpacity, StyleProp, ViewStyle } from 'react-native';
 
+import TabHeaderSearchBar from './TabHeaderSearchBar';
+
+import { SearchContext } from '@/context/SearchContext';
 import { cn } from '@/lib/utils';
 
 export type TabHeaderItemProps = BottomTabHeaderProps & {
   leftNav?: ReactNode;
   rightNav?: ReactNode;
+  showSearch?: boolean;
 };
 
 const iconSize = 20;
@@ -18,11 +22,20 @@ const navHeight = 2 * padding + logoHeight;
 
 const iconColor = '#333';
 
-export default function TabHeaderItem({ leftNav, rightNav }: TabHeaderItemProps) {
+export default function TabHeaderItem({ leftNav, rightNav, showSearch }: TabHeaderItemProps) {
+  const { search, handleSearch } = useContext(SearchContext);
+  const [searchText, setSearchText] = useState(search);
+  const [openSearch, setOpenSearch] = useState(false);
+
   const iconStyles: StyleProp<ViewStyle> = {
     paddingVertical: padding,
     paddingHorizontal: padding + 5,
   };
+
+  function updateSearch(text: string | null) {
+    setSearchText(text);
+    handleSearch(text);
+  }
 
   return (
     <SafeAreaView
@@ -33,17 +46,43 @@ export default function TabHeaderItem({ leftNav, rightNav }: TabHeaderItemProps)
       <View
         className="w-full flex-row items-center justify-between gap-3"
         style={{ marginTop: Platform.OS === 'android' ? 30 : 0, height: navHeight }}>
-        <View className="flex flex-[2] flex-row items-center justify-start gap-1">
-          <TouchableOpacity onPress={() => router.back()} style={iconStyles}>
-            <Feather name="arrow-left" size={iconSize} color={iconColor} />
-          </TouchableOpacity>
+        {openSearch ? (
+          <TabHeaderSearchBar
+            onBackPressed={() => {
+              updateSearch(null);
+              setOpenSearch(false);
+            }}
+            logoHeight={logoHeight}
+            padding={padding}
+            iconStyles={iconStyles}
+            iconColor={iconColor}
+            iconSize={iconSize}
+            updateSearch={updateSearch}
+            searchText={searchText}
+          />
+        ) : (
+          <>
+            <View className="flex flex-[2] flex-row items-center justify-start gap-1">
+              <TouchableOpacity onPress={() => router.back()} style={iconStyles}>
+                <Feather name="arrow-left" size={iconSize} color={iconColor} />
+              </TouchableOpacity>
 
-          {leftNav}
-        </View>
+              {leftNav}
+            </View>
 
-        <View className="flex flex-[1] flex-row items-center justify-end gap-3 px-5">
-          {rightNav}
-        </View>
+            <View
+              className="flex flex-[1] flex-row items-center justify-end gap-3"
+              style={{ paddingHorizontal: iconStyles.paddingHorizontal }}>
+              {rightNav}
+
+              {showSearch && (
+                <TouchableOpacity onPress={() => setOpenSearch(true)}>
+                  <Ionicons name="search" color={iconColor} size={iconSize} />
+                </TouchableOpacity>
+              )}
+            </View>
+          </>
+        )}
       </View>
     </SafeAreaView>
   );
