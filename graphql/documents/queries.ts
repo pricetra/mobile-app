c2ca7c9b3dbd146791ef9cc89f9652656bd0fb36
ayaanqui <ayaanqui.com@gmail.com>
2025-10-29 00:39:40 -0500

    feat: location param for `barcodeScan` query

--- graphql/documents/queries.ts
+++ graphql/documents/queries.ts
@@ -24,8 +24,8 @@ export const GET_ALL_COUNTRIES_QUERY = gql`
 `;
 
 export const BARCODE_SCAN_QUERY = gql`
-  query BarcodeScan($barcode: String!, $searchMode: Boolean) {
-    barcodeScan(barcode: $barcode, searchMode: $searchMode) {
+  query BarcodeScan($barcode: String!, $searchMode: Boolean, $location: LocationInput) {
+    barcodeScan(barcode: $barcode, searchMode: $searchMode, location: $location) {
       id
       name
       image
@@ -46,6 +46,13 @@ export const BARCODE_SCAN_QUERY = gql`
       quantityType
       createdAt
       updatedAt
+      stock {
+        id
+        productId
+        storeId
+        branchId
+        latestPriceId
+      }
     }
   }
 `;
