8289495dbf449178636dca6d6d01fc72e4b914c6
ayaanqui <ayaanqui.com@gmail.com>
2025-11-06 21:13:04 -0600

    feat: brand filter

--- components/ui/SearchFilters.tsx
+++ components/ui/SearchFilters.tsx
@@ -1,7 +1,12 @@
 import { AntDesign, Ionicons } from '@expo/vector-icons';
+import { useState } from 'react';
 import { TouchableOpacity, View, Text } from 'react-native';
 
+import FilterBrandsModal from './FilterBrandsModal';
+import ModalFormFull from './ModalFormFull';
+
 import { SearchRouteParams } from '@/app/(tabs)/search';
+import { cn } from '@/lib/utils';
 
 export type SearchFiltersProps = {
   onUpdateParams: (p: URLSearchParams) => void;
@@ -9,56 +14,90 @@ export type SearchFiltersProps = {
 };
 
 export default function SearchFilters({ params, onUpdateParams }: SearchFiltersProps) {
+  const [brandSelectionModal, setBrandSelectionModal] = useState(false);
+
   return (
     <View className="flex flex-row flex-wrap items-center gap-3">
-      {(params.category || params.categoryId) && (
-        <SearchFilterButton
-          label="Category"
-          value={params.category}
-          dropDown
-          onPressClear={() => {
-            const sp = new URLSearchParams(params);
-            sp.delete('category');
-            sp.delete('categoryId');
-            onUpdateParams(sp);
-          }}
-        />
-      )}
-
-      {params.brand && (
-        <SearchFilterButton
-          label="Brand"
-          value={params.brand}
-          onPressClear={() => {
-            const sp = new URLSearchParams(params);
-            sp.delete('brand');
-            onUpdateParams(sp);
-          }}
-        />
-      )}
-
-      {params.sortByPrice && (
-        <SearchFilterButton
-          label="Sort"
-          value={params.sortByPrice === 'asc' ? '↓ Price' : '↑ Price'}
-          onPressClear={() => {
-            const sp = new URLSearchParams(params);
-            sp.delete('sortByPrice');
-            onUpdateParams(sp);
-          }}
-        />
-      )}
-
-      {params.sale && (
-        <SearchFilterButton
-          label="Sale"
-          onPressClear={() => {
+      <SearchFilterButton
+        label="Sale"
+        onPress={() => {
+          if (params.sale) {
             const sp = new URLSearchParams(params);
             sp.delete('sale');
             onUpdateParams(sp);
+            return;
+          }
+          const sp = new URLSearchParams(params);
+          sp.set('sale', 'true');
+          onUpdateParams(sp);
+        }}
+        active={!!params.sale}
+      />
+
+      <SearchFilterButton
+        label="Category"
+        value={params.category || params.categoryId ? params.category : undefined}
+        dropDown
+        onPressClear={
+          params.category || params.categoryId
+            ? () => {
+                const sp = new URLSearchParams(params);
+                sp.delete('category');
+                sp.delete('categoryId');
+                onUpdateParams(sp);
+              }
+            : undefined
+        }
+      />
+
+      <SearchFilterButton
+        label="Brand"
+        value={params.brand ?? undefined}
+        dropDown
+        onPress={() => setBrandSelectionModal(true)}
+        onPressClear={
+          params.brand
+            ? () => {
+                const sp = new URLSearchParams(params);
+                sp.delete('brand');
+                onUpdateParams(sp);
+              }
+            : undefined
+        }
+      />
+
+      <ModalFormFull
+        title="Filter Brands"
+        onRequestClose={() => setBrandSelectionModal(false)}
+        visible={brandSelectionModal}>
+        <FilterBrandsModal
+          onSubmit={(brand) => {
+            const sp = new URLSearchParams(params);
+            sp.set('brand', brand);
+            onUpdateParams(sp);
+            setBrandSelectionModal(false);
           }}
+          onClose={() => setBrandSelectionModal(false)}
+          value={params.brand ?? undefined}
         />
-      )}
+      </ModalFormFull>
+
+      <SearchFilterButton
+        label="Sort"
+        value={
+          params.sortByPrice ? (params.sortByPrice === 'asc' ? '↓ Price' : '↑ Price') : undefined
+        }
+        dropDown
+        onPressClear={
+          params.sortByPrice
+            ? () => {
+                const sp = new URLSearchParams(params);
+                sp.delete('sortByPrice');
+                onUpdateParams(sp);
+              }
+            : undefined
+        }
+      />
     </View>
   );
 }
@@ -67,17 +106,23 @@ function SearchFilterButton({
   label,
   value,
   dropDown = false,
+  active = false,
   onPress,
   onPressClear,
 }: {
   label: string;
   value?: string;
   dropDown?: boolean;
+  active?: boolean;
   onPress?: () => void;
   onPressClear?: () => void;
 }) {
   return (
-    <View className="flex flex-row items-center gap-2 rounded-full bg-gray-100">
+    <View
+      className={cn(
+        'flex flex-row items-center gap-2 rounded-full',
+        active ? 'bg-green-200' : 'bg-gray-100'
+      )}>
       <TouchableOpacity className="flex flex-row items-center gap-1.5 py-2 pl-4" onPress={onPress}>
         <Text className="text-sm">{label}</Text>
         {value && <Text className="text-sm font-bold">{value}</Text>}
