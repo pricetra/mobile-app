829ee6cd3ae25b6751f0e3985131303871ef9fa9
ayaanqui <ayaanqui.com@gmail.com>
2025-11-06 18:47:29 -0600

    feat: search filter buttons

--- components/ui/SearchFilters.tsx
+++ components/ui/SearchFilters.tsx
@@ -12,77 +12,76 @@ export default function SearchFilters({ params, onUpdateParams }: SearchFiltersP
   return (
     <View className="flex flex-row flex-wrap items-center gap-3">
       {(params.category || params.categoryId) && (
-        <View className="flex flex-row items-center gap-3 rounded-full bg-gray-100 px-4 py-2">
-          <Text className="text-sm">
-            <Text>Category: </Text>
-            <Text className="font-bold">{params.category}</Text>
-          </Text>
-
-          <TouchableOpacity
-            onPress={() => {
-              const sp = new URLSearchParams(params);
-              sp.delete('category');
-              sp.delete('categoryId');
-              onUpdateParams(sp);
-            }}>
-            <AntDesign name="close" size={13} color="black" />
-          </TouchableOpacity>
-        </View>
+        <SearchFilterButton
+          label="Category"
+          value={params.category}
+          onPressClear={() => {
+            const sp = new URLSearchParams(params);
+            sp.delete('category');
+            sp.delete('categoryId');
+            onUpdateParams(sp);
+          }}
+        />
       )}
 
       {params.brand && (
-        <View className="flex flex-row items-center gap-3 rounded-full bg-gray-100 px-4 py-2">
-          <Text className="text-sm">
-            <Text>Brand: </Text>
-            <Text className="font-bold">{params.brand}</Text>
-          </Text>
-
-          <TouchableOpacity
-            onPress={() => {
-              const sp = new URLSearchParams(params);
-              sp.delete('brand');
-              onUpdateParams(sp);
-            }}>
-            <AntDesign name="close" size={13} color="black" />
-          </TouchableOpacity>
-        </View>
+        <SearchFilterButton
+          label="Brand"
+          value={params.brand}
+          onPressClear={() => {
+            const sp = new URLSearchParams(params);
+            sp.delete('brand');
+            onUpdateParams(sp);
+          }}
+        />
       )}
 
       {params.sortByPrice && (
-        <View className="flex flex-row items-center gap-3 rounded-full bg-gray-100 px-4 py-2">
-          <Text className="text-sm">
-            <Text>Sort by price: </Text>
-            <Text className="font-bold">{params.sortByPrice}</Text>
-          </Text>
-
-          <TouchableOpacity
-            onPress={() => {
-              const sp = new URLSearchParams(params);
-              sp.delete('sortByPrice');
-              onUpdateParams(sp);
-            }}>
-            <AntDesign name="close" size={13} color="black" />
-          </TouchableOpacity>
-        </View>
+        <SearchFilterButton
+          label={params.sortByPrice === 'asc' ? 'Lowest price' : 'Highest price'}
+          onPressClear={() => {
+            const sp = new URLSearchParams(params);
+            sp.delete('sortByPrice');
+            onUpdateParams(sp);
+          }}
+        />
       )}
 
       {params.sale && (
-        <View className="flex flex-row items-center gap-3 rounded-full bg-gray-100 px-4 py-2">
-          <Text className="text-sm">
-            <Text>Sale: </Text>
-            <Text className="font-bold">{params.sale}</Text>
-          </Text>
-
-          <TouchableOpacity
-            onPress={() => {
-              const sp = new URLSearchParams(params);
-              sp.delete('sale');
-              onUpdateParams(sp);
-            }}>
-            <AntDesign name="close" size={13} color="black" />
-          </TouchableOpacity>
-        </View>
+        <SearchFilterButton
+          label="Sale"
+          onPressClear={() => {
+            const sp = new URLSearchParams(params);
+            sp.delete('sale');
+            onUpdateParams(sp);
+          }}
+        />
       )}
+    </View>
+  );
+}
+
+function SearchFilterButton({
+  label,
+  value,
+  onPress,
+  onPressClear,
+}: {
+  label: string;
+  value?: string;
+  onPress?: () => void;
+  onPressClear: () => void;
+}) {
+  return (
+    <View className="flex flex-row items-center gap-2 rounded-full bg-gray-100">
+      <TouchableOpacity className="flex flex-row items-center gap-1 py-2 pl-4" onPress={onPress}>
+        <Text className="text-sm">{label}</Text>
+        {value && <Text className="text-sm font-bold">{value}</Text>}
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={onPressClear} className="p-1.5 pr-4">
+        <AntDesign name="close" size={13} color="black" />
+      </TouchableOpacity>
     </View>
   );
 }
