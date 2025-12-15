bc49a0cd11a429eca9edce097542c9bc6d666bf2
ayaanqui <ayaanqui.com@gmail.com>
2025-12-14 21:28:20 -0600

    feat: implement google oauth on login and signup screens

--- components/auth/Register.tsx
+++ components/auth/Register.tsx
@@ -1,4 +1,5 @@
-import { useMutation } from '@apollo/client';
+import { useLazyQuery, useMutation } from '@apollo/client';
+import { CreateAccountDocument, GoogleOAuthDocument } from 'graphql-utils';
 import { useContext, useEffect, useRef, useState } from 'react';
 import { Text, Alert, TextInput } from 'react-native';
 
@@ -6,14 +7,26 @@ import Input from '../ui/Input';
 
 import AuthFormContainer from '@/components/auth/ui/AuthFormContainer';
 import { AuthModalContext, AuthScreenType } from '@/context/AuthModalContext';
-import { CreateAccountDocument } from 'graphql-utils';
+import { useGoogleAuth } from '@/hooks/useGoogleAuth';
+import { JwtStoreContext } from '@/context/JwtStoreContext';
 
 export default function RegisterScreen() {
+  const { updateJwt } = useContext(JwtStoreContext);
   const { setScreen } = useContext(AuthModalContext);
   const [createAccount, { data, loading, error }] = useMutation(CreateAccountDocument);
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+
+  const {
+    signIn: startGoogleAuth,
+    loading: googleAuthLoading,
+    accessToken: googleAccessToken,
+  } = useGoogleAuth();
+  const [googleOauth, { loading: googleOauthLoading }] = useLazyQuery(GoogleOAuthDocument, {
+    fetchPolicy: 'no-cache',
+  });
+  const googleAuthInProgress = googleAuthLoading || googleOauthLoading;
 
   const fullNameInputRef = useRef<TextInput>(null);
   const passwordInputRef = useRef<TextInput>(null);
@@ -26,6 +39,21 @@ export default function RegisterScreen() {
   async function onSignUp() {
     createAccount({ variables: { email, password, name } });
   }
+
+  useEffect(() => {
+    if (!googleAccessToken) return;
+
+    googleOauth({
+      variables: {
+        accessToken: googleAccessToken,
+      },
+    })
+      .then(({ data }) => {
+        if (!data) return;
+        updateJwt(data.googleOAuth.token);
+      })
+      .catch((err) => Alert.alert('Could not complete Google OAuth', err));
+  }, [googleAccessToken]);
 
   return (
     <AuthFormContainer
@@ -51,12 +79,8 @@ export default function RegisterScreen() {
           'Sign in with Apple is currently not available on the mobile app'
         )
       }
-      onPressGoogle={() =>
-        Alert.alert(
-          'Login method not implemented',
-          'Sign in with Google is currently not available on the mobile app'
-        )
-      }
+      googleLoading={googleAuthInProgress}
+      onPressGoogle={startGoogleAuth}
       onPressSubmit={onSignUp}>
       <Input
         label="Email"
