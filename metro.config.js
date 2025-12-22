447aa30f424e141e42e94de6796ae437674a74c7
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2025-12-22 01:23:35 -0600

    build: revert babel and metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,5 @@ const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
-config.resolver.extraNodeModules = {
-  ...(config.resolver.extraNodeModules || {}),
-  assert: require.resolve('assert'),
-};
+
 module.exports = withNativeWind(config, { input: './global.css' });
